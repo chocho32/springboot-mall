b3e717e6ff4bd996bf35ed9f79d9bca064ad99f1
jamescho <jamesccl32@gmail.com>
2023-12-17 23:39:02 +0800

    user function - check register email and hide response password

--- src/main/java/com/cho/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/cho/springbootmall/service/impl/UserServiceImpl.java
@@ -5,11 +5,17 @@ import com.cho.springbootmall.dao.UserDao;
 import com.cho.springbootmall.dto.UserRegisterRequest;
 import com.cho.springbootmall.model.User;
 import com.cho.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -21,6 +27,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        //check register email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null){
+            log.warn("Email {} has been registered", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //register email
         return userDao.createUser(userRegisterRequest);
     }
 }
